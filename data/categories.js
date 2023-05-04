8a994bd5c7fbdddbabf0e4aec7c8cb30e3ef912c
vivi <viviana.calva@lumation.co>
2023-05-03 21:53:07 -0500

    Actualizando carga de data de prueba

--- data/categories.js
+++ data/categories.js
@@ -1,9 +1,12 @@
 // name
 
 module.exports = [
-   { name: "aceites" },
-   { name: "lacteos" },
-   { name: "arroz" },
-   { name: "harina" },
-   { name: "snacks" },
-]
+  { name: "aceites" },
+  { name: "lacteos" },
+  { name: "arroz" },
+  { name: "harina" },
+  { name: "snacks" },
+  { name: "jugos" },
+  { name: "bebidas" },
+  { name: "mantequillas" },
+];
--- db.js
+++ db.js
@@ -1,27 +1,22 @@
-const mongoose = require('mongoose');
-const { DB: { MONGO_URL } } = require('./config');
-// const Product = require('./models/Product');
-// const PRODUCTS = require("./data/products");
-// const Category = require('./models/Category');
-// const CATEGORIES = require("./data/categories");
+const mongoose = require("mongoose");
+const {
+  DB: { MONGO_URL },
+} = require("./config");
+const loadData = require("./helpers/loadData");
 
 const connectDB = async () => {
-   try {
-      const connection = await mongoose.connect(MONGO_URL, {
-         useNewUrlParser: true,
-         useUnifiedTopology: true
-      });
+  try {
+    const connection = await mongoose.connect(MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-      console.log(`MongoDB Connected: ${connection.connection.host}`)
-      // let categories = CATEGORIES.map(e => new Category(e));
-      // await Category.bulkSave(categories);
-      // let products = PRODUCTS.map(e => new Product(e));
-      // await Product.bulkSave(products);
-   } catch (err) {
-      console.log(`No ha sido posible realizar una conexión con la BBDD`);
-      console.log(`Error: ${err.message}`);
-   }
-}
-
+    console.log(`MongoDB Connected: ${connection.connection.host}`);
+    await loadData();
+  } catch (err) {
+    console.log("No ha sido posible realizar una conexión con la BBDD");
+    console.log(`Error: ${err.message}`);
+  }
+};
 
 module.exports = connectDB;
